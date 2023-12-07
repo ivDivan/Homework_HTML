0162ef9f2e21f105845ce65e73e2faaf9b9d31fc
ivDivan <150170769+ivDivan@users.noreply.github.com>
2023-12-07 11:26:21 +0100

    Add files via upload

---
+++ Zodiac homework/script.js
@@ -0,0 +1,59 @@
+let input = prompt("Gimme your number");
+let year = parseInt(input);
+
+let modulo = (year-4)%12
+
+if(modulo === 0) {
+    console.log("You rat!!!")
+}
+else if(modulo === 1) {
+    console.log("Stubborn as an ox!!!")
+}
+else if(modulo === 2) {
+    console.log("Roooaaaar!!!")
+}
+else if(modulo === 3) {
+    console.log("Holy hand grenade!!!")
+}
+else if(modulo === 4) {
+    console.log("Winter is coming!!!")
+}
+else if(modulo === 5) {
+    console.log("Alexis!!!")
+}
+else if(modulo === 6) {
+    console.log("Neeeigh!!!")
+}
+else if(modulo === 7) {
+    console.log("Beh s\'s kozite!!!")
+}
+else if(modulo === 8) {
+    console.log("Monkey see!!!")
+}
+else if(modulo === 9) {
+    console.log("A doodle doo!!!")
+}
+else if(modulo === 10) {
+    console.log("Woof!!!")
+}
+else if(modulo === 11) {
+    console.log("Bacon!!!")
+}
+else {
+    console.log("Don\'t mess around! Gimme a number!!")
+}
+
+// 0 - Rat
+// 1 - Ox
+// 2 - Tiger
+// 3 - Rabbit
+// 4 - Dragon
+// 5 - Snake
+// 6 - Horse
+// 7 - Goat
+// 8 - Monkey
+// 9 - Rooster
+// 10 - Dog
+// 11 - Pig
+
+
